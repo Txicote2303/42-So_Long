ff61eda44cbe6a0cbc9b558ed5b21960f525ebb5
Txicote <xicoleite23@gmail.com>
2022-11-18 17:51:32 +0000

    auto-commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# 42-So_Long
-Complete So_Long project, including the project PDF's (Currently under Dev)
--- funcs/structures.c
+++ funcs/structures.c
@@ -6,7 +6,7 @@
 /*   By: frcarval <frcarval@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 21:31:23 by frcarval          #+#    #+#             */
-/*   Updated: 2022/10/27 13:36:53 by frcarval         ###   ########.fr       */
+/*   Updated: 2022/11/15 14:51:19 by frcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ t_map	*map(void)
 	
 	return (&var);
 }
+
 t_mapcp	*mapcp(void)
 {
 	static	t_mapcp var;
 	
 	return (&var);
 }
+
 t_window	*window(void)
 {
 	static	t_window var;
 	
 	return (&var);
 }
+
 t_img	*img(void)
 {
 	static	t_img var;
